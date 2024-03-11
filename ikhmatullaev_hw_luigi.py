21afdb8d01bf6dd85924d405d4fcede9f07e5928
Davron <ihmatullaev@gmail.com>
2024-03-11 18:47:59 +0500

    decorator requires

--- ikhmatullaev_hw_luigi.py
+++ ikhmatullaev_hw_luigi.py
@@ -6,6 +6,7 @@ import os
 import tarfile
 import io
 import pandas as pd
+from luigi.util import requires
 
 
 logger = logging.getLogger('luigi-interface')
@@ -16,8 +17,7 @@ class DownloadDataset(luigi.Task):
     link = 'https://www.ncbi.nlm.nih.gov/geo/download/?acc={dataset_name}&format=file'
     dataset_name = luigi.Parameter(default='GSE68849')
     TARGET_PATH = '/home/fitlemon/code/luigi/data'
-    
-    
+        
     def output(self):
         return luigi.LocalTarget(f'{self.TARGET_PATH}/{self.dataset_name}.tar')
     
@@ -33,12 +33,18 @@ class DownloadDataset(luigi.Task):
             logger.info(f'Saved archive to {self.output()}') 
 
 
+@requires(DownloadDataset)
 class ExtractArchives(luigi.Task):
     # Извлечение архивов из главного архива .tar
-    dataset_name = luigi.Parameter(default="GSE68849")
-
-    def requires(self):
-        return DownloadDataset(dataset_name=self.dataset_name)
+    
+    def output(self):
+        if self.input():
+            tar_file = self.input().path
+            if os.path.exists(tar_file):
+                tarf = tarfile.open(tar_file)
+                files = tarf.getnames()
+                return (luigi.LocalTarget(f"data/{self.dataset_name}/archives"), 
+                        [luigi.LocalTarget(f"data/{self.dataset_name}/archives/{file_name}") for file_name in files])
 
     def run(self):
         logger.info(f"Creating directory {self.output()[0].path}...")
@@ -48,20 +54,26 @@ class ExtractArchives(luigi.Task):
             archive.extractall(path=self.output()[0].path)
         logger.info(f"Files from tar archive extracted into {self.output()[0].path}.")
 
-    def output(self):
-        if self.input():
-            tar_file = self.input().path
-            if os.path.exists(tar_file):
-                tarf = tarfile.open(tar_file)
-                files = tarf.getnames()
-                return (luigi.LocalTarget(f"data/{self.dataset_name}/archives"), [luigi.LocalTarget(f"data/{self.dataset_name}/archives/{file_name}") for file_name in files])
-        
+
+@requires(ExtractArchives)        
 class ExtractFiles(luigi.Task):
     # Извлечение файлов из архивов .gz
-    dataset_name = luigi.Parameter(default="GSE68849")
-
-    def requires(self):
-        return ExtractArchives(dataset_name=self.dataset_name)
+    
+    def output(self):
+        if self.input():
+            extracted_dir = self.input()[0].path
+            parent_dir = os.path.dirname(extracted_dir)      
+            target_dir = os.path.join(parent_dir, 'files') 
+            # Получить список сжатых gz файлов
+            input_files = [file.path for file in self.input()[1]]
+            output_files = []
+            for file in input_files:
+                if file.endswith('.gz'):
+                    base_name = os.path.basename(file).rsplit('.', 2)[0]
+                    target_path = os.path.join(target_dir, base_name, os.path.basename(file)[:-3])
+                    # Сохраним название файла в архиве gz в список извлекаемых файлов
+                    output_files.append(target_path)
+            return (luigi.LocalTarget(target_dir), [luigi.LocalTarget(file_name) for file_name in output_files])
     
     def run(self):
         target_dir = self.output()[0].path
@@ -75,39 +87,12 @@ class ExtractFiles(luigi.Task):
             subprocess.run(['gunzip', '-c', input], stdout=open(output, 'wb'))
             logger.info(f"Unzipped archive to folder {base_name}.")
                     
-    def output(self):
-        if self.input():
-            extracted_dir = self.input()[0].path
-            print(extracted_dir) 
-            parent_dir = os.path.dirname(extracted_dir)
-            print(parent_dir)        
-            target_dir = os.path.join(parent_dir, 'files')
-            print(target_dir)  
-            # Получить список сжатых gz файлов
-            input_files = [file.path for file in self.input()[1]]
-            output_files = []
-            for file in input_files:
-                if file.endswith('.gz'):
-                    base_name = os.path.basename(file).rsplit('.', 2)[0]
-                    target_path = os.path.join(target_dir, base_name, os.path.basename(file)[:-3])
-                    # Сохраним название файла в архиве gz в список извлекаемых файлов
-                    output_files.append(target_path)
-            return (luigi.LocalTarget(target_dir), [luigi.LocalTarget(file_name) for file_name in output_files])
+
     
-    
+@requires(ExtractFiles)   
 class SplitTables(luigi.Task):
     # Парсинг текстовых файлов и извлечение таблиц
-    dataset_name = luigi.Parameter(default="GSE68849")
-
-    def requires(self):
-        return ExtractFiles(dataset_name=self.dataset_name)
     
-    def run(self):
-        input_files = [file.path for file in self.input()[1]]
-        for file in input_files:
-            logger.info(f"Split into tables the file {file}.")
-            self._split_into_tables(file)
-            
     def output(self):
         if self.input():
             tables = ['Columns.tsv', 'Controls.tsv', 'Heading.tsv', 'Probes.tsv'] # допустим я знаю, что должны быть извлечены именно эти таблицы с каждого файла
@@ -120,7 +105,13 @@ class SplitTables(luigi.Task):
                     # Сохраним название файла-таблицы в список
                     output_files.append(output_file)
             return [luigi.LocalTarget(file) for file in output_files]
-    
+        
+    def run(self):
+        input_files = [file.path for file in self.input()[1]]
+        for file in input_files:
+            logger.info(f"Split into tables the file {file}.")
+            self._split_into_tables(file)
+                
     def _split_into_tables(self, file_path):
         dfs = {}
         with open(file_path) as f:
@@ -157,14 +148,11 @@ class SplitTables(luigi.Task):
         os.makedirs(os.path.dirname(full_path), exist_ok=True)
         # Сохраняем DataFrame
         df.to_csv(full_path, sep='\t', index=False)
-    
-    
+
+
+@requires(SplitTables)
 class ProcessProbesTable(luigi.Task):
     # Обработка таблицы Probes и сохранения урезанной версии.
-    dataset_name = luigi.Parameter(default="GSE68849")
-
-    def requires(self):
-        return SplitTables(dataset_name=self.dataset_name)
 
     def output(self):
         if self.input():
@@ -177,21 +165,18 @@ class ProcessProbesTable(luigi.Task):
         for file in input_files:
             if os.path.basename(file) == 'Probes.tsv':
                 logger.info(f"Modify Probes dataframe dataframe.")
-                self._modify_df(file)
+                self._modify_table(file)
                     
-    def _modify_df(self, file_path):
+    def _modify_table(self, file_path):
         df = pd.read_csv(file_path, sep='\t')
         df = df.drop(columns=['Definition', 'Ontology_Component', 'Ontology_Process', 'Ontology_Function', 'Synonyms', 'Obsolete_Probe_Id', 'Probe_Sequence'])
         new_file_path = str.replace(file_path, 'Probes', 'Probes_trimmed')
         df.to_csv(new_file_path, sep='\t', index=False)
         
 
+@requires(ProcessProbesTable)
 class CheckAndClear(luigi.Task):
     # Удалить исходные файлы txt
-    dataset_name = luigi.Parameter(default="GSE68849")
-    
-    def requires(self):
-        return ProcessProbesTable(dataset_name = self.dataset_name)
     
     def run(self):
         input_files = [file.path for file in self.input()]
